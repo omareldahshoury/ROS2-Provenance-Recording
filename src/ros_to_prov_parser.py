37e9f6e9e2bdeffeb02a6b52e7d3e1c28411cdda
Syed Ibrahim Shakir <omarsrstt@gmail.com>
2021-06-08 23:19:23 +0200

    ros_parser node data separated to a dictionary

--- src/ros_to_prov_parser.py
+++ src/ros_to_prov_parser.py
@@ -65,7 +65,34 @@ class Agent:
 
     # This function parses data from the node_ files and extracts relevant data
     def agent_info_parser(data):
-        print('Hi, I have reached this function')
+        # print('Hi, I have reached this function')
+        # print(data)
+        # First we extract the name of the node
+        UID = data.pop(0).strip(" /")
+        print(UID)
+        
+        # We create a reference dictionary which stores the order of keys to be read
+        # If we have to extract additional info, it can be added here
+        extract_dict = {'Subscribers': None, 'Publishers': None, 'Services': None}
+        current_key, li = "Nothing", []
+        # We cycle through the data
+        for _ in range(len(data)):
+            # For the data, we check if it is one of the keys
+            info = data[0].strip(" :")
+            if info in extract_dict.keys():
+                # We store all the data which is related to the key
+                extract_dict[current_key] = li
+                # We then change the key to the next input
+                current_key = data.pop(0).strip(" :")
+                # Finally, we reset the key
+                li = []
+            else:
+                li.append('_'.join(data.pop(0).strip(" /").split('/')))
+        # We store the data from the last key as well
+        extract_dict[current_key] = li
+        print(extract_dict, "\n\n")
+                
+
         # data_li = data.rstrip("\n ").split("\n")
         # UID = data_li[0].split(" ")[-1].lstrip("/")
         # no_of_pubs = int(data_li[1].split(" ")[-1])
