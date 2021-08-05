e67c242d1bd23d7877cac95bd16868ede209db56
Syed Ibrahim Shakir <omarsrstt@gmail.com>
2021-08-05 17:13:13 +0200

    Relevant data extraction model completed and stored in nested dict

--- src/single_instance_prov_generator.py
+++ src/single_instance_prov_generator.py
@@ -6,6 +6,8 @@
 import rclpy
 # Used to add timestamp information to arrange the datapoints sequentially
 from datetime import datetime
+# To introduce small delays
+import time
 
 
 def extract_ros_info(recording_node):
@@ -24,8 +26,11 @@ def extract_ros_info(recording_node):
     # We initialize an empty dictionary, this will store all the relevant information
     ros_info = {current_time: {'nodes': {}, 'topics':{}}}
     
+    # Exlcusions
     # We exclude certain nodes from our database which we don't want to record
     excluded_nodes = [recording_node.get_name()]
+    # We could do the same with topics
+    excluded_topics = []
 
     # We then retrieve the list of all the nodes in the system along with their namespaces
     nodes_n_namespaces = recording_node.get_node_names_and_namespaces()
@@ -33,14 +38,26 @@ def extract_ros_info(recording_node):
     for node, namespace in nodes_n_namespaces:
         # We don't want to save the recorder node as it isn't part of the system
         if node not in excluded_nodes:
+            # We get the Publishing relations for each node
+            list_of_pubs = recording_node.get_publisher_names_and_types_by_node(node, namespace)
+            # We then get the Subscriptions for each node
+            list_of_subs = recording_node.get_subscriber_names_and_types_by_node(node, namespace)
+            # Then we finally store them
             ros_info[current_time]['nodes'][node] = {'name': node,
                                                     'namespace': namespace,
-                                                    'publishes_to': "",
-                                                    'subscribes_to': ""
+                                                    'publishes_to': list_of_pubs,
+                                                    'subscribes_to': list_of_subs
                                                     }
 
-    
+    # We then get the list of all the topics
+    topic_list = recording_node.get_topic_names_and_types()
+    for topic, type in topic_list:
+        if topic not in excluded_topics:
+            ros_info[current_time]['topics'][topic] = { 'name': topic,
+                                                        'type': type[0]
+                                                        }
 
+    # Finally, we return the data structure
     return ros_info
 
 if __name__ == "__main__":
@@ -51,6 +68,8 @@ if __name__ == "__main__":
     # Here we create a node which is used to record the ROS System
     r2p = rclpy.create_node('ros2prov_recorder')
 
+    # 100 millisec time to let the system setup
+    time.sleep(0.1)
+
     ros_info = extract_ros_info(r2p)
     print(ros_info)
-
